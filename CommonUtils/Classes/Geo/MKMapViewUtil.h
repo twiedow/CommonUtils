b042f8c70cc62e90d5549b77626a1c82cfee3513
Tobias Wiedow <tobi@wiedow.biz>
2014-03-15 20:06:33 +0100

    adds map view util

---
+++ CommonUtils/Classes/Geo/MKMapViewUtil.h
@@ -0,0 +1,15 @@
+//
+//  MKMapViewUtil.h
+//  CommonUtils
+//
+//  Created by twiedow on 15.03.14.
+//  Copyright (c) 2014 twiedow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MKMapViewUtil : NSObject
+
++ (void)zoomToFitMapAnnotations:(MKMapView *)mapView;
+
+@end
---
+++ CommonUtils/Classes/Geo/MKMapViewUtil.m
@@ -0,0 +1,45 @@
+//
+//  MKMapViewUtil.m
+//  CommonUtils
+//
+//  Created by twiedow on 15.03.14.
+//  Copyright (c) 2014 twiedow. All rights reserved.
+//
+
+#import "MKMapViewUtil.h"
+
+@import MapKit;
+
+
+@implementation MKMapViewUtil
+
++ (void)zoomToFitMapAnnotations:(MKMapView *)mapView {
+	if ([mapView.annotations count]==0)
+		return;
+
+	CLLocationCoordinate2D topLeftCoord = CLLocationCoordinate2DMake(-90, 180);
+	CLLocationCoordinate2D bottomRightCoord = CLLocationCoordinate2DMake(90, -180);
+
+	for (id<MKAnnotation> annotation in mapView.annotations) {
+		topLeftCoord.longitude=fmin(topLeftCoord.longitude, annotation.coordinate.longitude);
+		topLeftCoord.latitude=fmin(topLeftCoord.latitude, annotation.coordinate.latitude);
+
+		bottomRightCoord.longitude=fmax(bottomRightCoord.longitude, annotation.coordinate.longitude);
+		bottomRightCoord.latitude=fmax(bottomRightCoord.latitude, annotation.coordinate.latitude);
+	}
+
+	MKCoordinateRegion region;
+	region.center.latitude=topLeftCoord.latitude - (topLeftCoord.latitude - bottomRightCoord.latitude) * 0.5;
+	region.center.longitude=topLeftCoord.longitude + (bottomRightCoord.longitude - topLeftCoord.longitude) * 0.5;
+	region.span.latitudeDelta=fabs(topLeftCoord.latitude - bottomRightCoord.latitude) * 1.3;
+	region.span.longitudeDelta=fabs(bottomRightCoord.longitude - topLeftCoord.longitude) * 1.1;
+
+	region=[mapView regionThatFits:region];
+
+  if (isnan(region.center.latitude) || isnan(region.center.longitude) || isnan(region.span.latitudeDelta) || isnan(region.span.longitudeDelta))
+		return;
+
+  [mapView setRegion:region animated:YES];
+}
+
+@end
